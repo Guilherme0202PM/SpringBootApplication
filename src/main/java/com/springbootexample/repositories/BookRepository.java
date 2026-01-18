c1274a2a970a3c60f937666949b6c036987bb922
Guilherme <guilhermeperes0202@gmail.com>
2026-01-18 20:21:21 -0300

    Up repositories and services

---
+++ src/main/java/com/springbootexample/repositories/BookRepository.java
@@ -0,0 +1,8 @@
+package com.springbootexample.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.springbootexample.models.Book;
+public interface BookRepository  extends MongoRepository<Book, String> {
+    
+}
